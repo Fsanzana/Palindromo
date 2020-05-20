af08e6f0608a3f4c86005632f81e498319321eeb
Luisburgoro <64493029+Luisburgoro@users.noreply.github.com>
2020-05-20 16:26:24 -0400

    aporte individual- Burgos, Robles, casos del profe

--- src/test/java/PalindromoTest.java
+++ src/test/java/PalindromoTest.java
@@ -13,16 +13,28 @@ public class PalindromoTest {
 
     @org.junit.Test
     public void esPalindromo() {
+        //Burgos
         assertTrue(p.esPalindromo("Anita lava la tina"));
         assertFalse(p.esPalindromo("gatito"));
         assertTrue(p.esPalindromo("alli ves a sevilla"));
         assertFalse(p.esPalindromo("anita no lava la tina"));
-        assertTrue(p.esPalindromo("anita lava la tína"));
-        assertFalse(p.esPalindromo("200"));
+        assertTrue(p.esPalindromo("4n1t4 l4v4 l4 t1n4"));
+       
+      
+        //Robles
         assertTrue(p.esPalindromo("1 anita lava la tina 1"));
         assertTrue(p.esPalindromo("añita lava la tiña"));
+        assertFalse(p.esPalindromo("All-í va Ram3ón y no mar4avi_lla"));
         assertFalse(p.esPalindromo("anita, lava, la, tina"));
-        assertTrue(p.esPalindromo(""));
         assertTrue(p.esPalindromo("°|!#$%&/()=?'¡¿´¨+*~}]`{[^,;.:-__-:.;,^[{`]}~*+¨´¿¡'?=)(/&%$#!|°"));
+       
+        
+        
+        //Profesor
+        assertFalse(p.esPalindromo("200"));
+        assertTrue(p.esPalindromo("aaabccbaaa"));
+        assertFalse(p.esPalindromo("ahabccbaaa"));
+        assertTrue(p.esPalindromo("La tele letal"));
+        assertTrue(p.esPalindromo(""));
     }
 }
